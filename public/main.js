218d5f1aefe0b6b37ffa5ec14e3c381d3b393dc1
Kelly Devine <kellyd813@gmail.com>
2015-07-20 20:47:33 -0400

    formatting/styling modal

--- public/main.js
+++ public/main.js
@@ -1,48 +1,48 @@
 (function () {
 
-    var app = angular.module('app', ['plangular'])
+  var app = angular.module('app', ['plangular'])
 
-    .filter('getTag', function() {
-        return function(str) {
-            return str.match(/(?:[^\s"]+|"[^"]*")+/g)[0].replace(/"/g, "");
-        };
-    })
+  .filter('getTag', function() {
+      return function(str) {
+          return str.match(/(?:[^\s"]+|"[^"]*")+/g)[0].replace(/"/g, "");
+      };
+  })
 
-    .filter('fixDate', function() {
-        return function(str) {
-            var ary = str.split(" ")[0].split("/");
-            ary.push(ary.shift());
-            return ary.join("/");
-        }
-    });
+  .filter('fixDate', function() {
+      return function(str) {
+          var ary = str.split(" ")[0].split("/");
+          ary.push(ary.shift());
+          return ary.join("/");
+      }
+  });
 
-    app.config(function(plangularConfigProvider){
-      plangularConfigProvider.clientId = "56dbf65179579b2fb83032d13230bbdf";
-    });
+  app.config(function(plangularConfigProvider){
+    plangularConfigProvider.clientId = "56dbf65179579b2fb83032d13230bbdf";
+  });
 
-    app.controller('TrackController', function($scope) {
-        $scope.sortType     = 'title';
-        $scope.sortType     = 'created_at';
-        $scope.sortType     = 'duration';
-        $scope.sortType     = 'src';
-        $scope.sortType     = 'tag_list';
-        $scope.sortReverse  = false;
-        $scope.searchPods   = '';
-    })
+  app.controller('TrackController', function($scope) {
+      $scope.sortType     = 'title';
+      $scope.sortType     = 'created_at';
+      $scope.sortType     = 'duration';
+      $scope.sortType     = 'src';
+      $scope.sortType     = 'tag_list';
+      $scope.sortReverse  = false;
+      $scope.searchPods   = '';
+  })
 
-    // Modal
+  // Modal
 
-    $('.PodQuest').click(function() {
-      $('.modal').toggleClass('is-open');
-    });
+  $('.PodQuest').click(function() {
+    $('.modal').toggleClass('is-open');
+  });
 
-    $('.close').click(function() {
-      $('.modal').removeClass('is-open');
-    });
+  $('.close').click(function() {
+    $('.modal').removeClass('is-open');
+  });
 
-    $('.modal').click(function() {
-      $('.modal').removeClass('is-open');
-    });
+  $('.modal').click(function() {
+    $('.modal').removeClass('is-open');
+  });
 
 })();
 
